307454e96b5871c25dd42411a00ce8a1ad427989
Serguei Ossokine <serguei.ossokine@tutanota.com>
2024-05-16 08:11:45 +0000

    Add script to fetch the needed data

---
+++ hw1/data/fetch_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Fetch the NYC yellow cab trip data for Jan and Feb of 2023
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-02.parquet
