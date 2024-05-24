c480ffef9f4b991c78460a0f9f08b6b0ba586405
Serguei Ossokine <serguei.ossokine@tutanota.com>
2024-05-24 11:32:17 +0200

    First attempt at HW2

---
+++ hw2/homework/data/fetch_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Fetch the NYC yellow cab trip data for Jan and Feb of 2023
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-01.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-02.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-03.parquet
